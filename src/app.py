4787d3b464fb6ed13b791aa6515942595fe3faab
jcanedo279 <52115366+jcanedo279@users.noreply.github.com>
2024-05-14 04:05:49 -0400

    Wire in the password reset and report routes in the backend, misc changes to support frontend updates.

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ import json
 import jwt
 from datetime import datetime, timedelta, timezone
 
-from app_util import BACKEND_PROPERTIES_DF, properties_df_from_search_request_data, properties_response_from_properties_df, env, Env
+from app_util import BACKEND_PROPERTIES_DF, properties_df_from_search_request_data, properties_response_from_properties_df, env, Env, REGION_TO_ZIP_CODE
 from email_service_util import email_app
 
 from flask_cors import CORS
@@ -94,9 +94,26 @@ users = {
     }
 }
 
-def search_properties(query_address):
-    # Filter the DataFrame for addresses that contain the query string, case-insensitive
-    return BACKEND_PROPERTIES_DF[BACKEND_PROPERTIES_DF['street_address'].str.contains(query_address, case=False, na=False)]
+
+#############
+## UTILITY ##
+#############
+
+def fancy_flash(message, status='info', flash_id='default', animation=None):
+    '''
+        A message based flash message with the following funcitonality:
+        - Regular flash functionality, i.e. message and message category.
+        - Multiple flash messages per html via area.
+        - Custom animation types.
+    '''
+    return {
+        'fancy_flash': [{
+            'message': message,
+            'status': status,
+            'flash_id': flash_id,
+            'animation': animation
+        }]
+    }
 
 
 #####################
@@ -118,54 +135,22 @@ class User(UserMixin):
 def load_user(user_email):
     return User(user_email)
 
-def fancy_flash(message, status='info', flash_id='default', animation=None):
-    '''
-        A message based flash message with the following funcitonality:
-        - Regular flash functionality, i.e. message and message category.
-        - Multiple flash messages per html via area.
-        - Custom animation types.
-    '''
-    return {
-        'fancy_flash': [{
-            'message': message,
-            'status': status,
-            'flash_id': flash_id,
-            'animation': animation
-        }]
-    }
-
-def properties_response(properties_df, num_properties_per_page=1, page=1, user_obj=None):
-    saved_zpids = set(user_obj.saved) if user_obj else {}
-    response_data = properties_response_from_properties_df(properties_df, num_properties_per_page=num_properties_per_page, page=page, saved_zpids=saved_zpids)
-    # If the user is logged out, add a description to log in.
-    if user_obj:
-        response_data['descriptions']['Save'] = 'Save/unsave this property to go back to it later.'
-    else:
-        response_data['descriptions']['Save'] = 'To save a property you must first login.'
-    return response_data
+def maybe_load_user(user_email):
+    user_obj = None
+    if user_email == 'anonymous':
+        app.logger.info('Anonymous user detected')
+    elif user_email:
+        app.logger.info('User is authenticated, skipping CSRF header validation.')
+        user_obj = load_user(user_email)
+    return user_obj
 
 
 #########################
 ## APP MAILING UTILITY ##
 #########################
 
-@celery.task(bind=True, retry_backoff=True, max_retries=3)
-def send_async_email(self, email_data):
-    ''' Background task to send an email using the provided email service. '''
-    try:
-        # Ensure a fresh connection for each email
-        with SMTP(email_app.smtp_server, email_app.smtp_port) as server:
-            if email_app.use_tls:
-                server.starttls()
-            server.login(email_app.sender_email, email_app.password)
-            # Use the server object for sending the email
-            if email_app.send_email(server, email_data['recipient'], email_data['subject'], email_data['body'], email_data['html']):
-                app.logger.info("Email sent successfully")
-            else:
-                raise Exception("Failed to send email")
-    except Exception as e:
-        app.logger.error(f'Failed to send email: {str(e)}')
-        self.retry(exc=e)
+def get_external_url(endpoint, **values):
+    return NGINX_URL + url_for(endpoint, **values)
 
 def generate_token(data, salt='generic-salt', expiration=3600):
     ''' Generate a secure token for a given data with a salt and expiration time. '''
@@ -181,59 +166,77 @@ def verify_token(token, salt='generic-salt', expiration=3600):
     except (SignatureExpired, BadSignature):
         return None
 
-def send_email_with_message(user_email, salt_key, redirect_url_method_name, email_subject, email_template, email_body):
-    token = generate_token(user_email, salt=salt_key, expiration=3600)
-    redirect_url = get_external_url(redirect_url_method_name, token=token)
-    body = render_template(email_body, redirect_url=redirect_url, user_email=user_email)
-    html = render_template(email_template, redirect_url=redirect_url, user_email=user_email)
+@celery.task(bind=True, retry_backoff=True, max_retries=3)
+def send_async_email(self, email_data):
+    ''' Background task to send an email using the provided email service. '''
+    try:
+        # Ensure a fresh connection for each email
+        with SMTP(email_app.smtp_server, email_app.smtp_port) as server:
+            if email_app.use_tls:
+                server.starttls()
+            server.login(email_app.sender_email, email_app.password)
+            # Use the server object for sending the email
+            if email_app.send_email(server, email_data['recipient'], email_data['subject'], email_data['body'], email_data['html']):
+                app.logger.info('Email sent successfully.')
+            else:
+                raise Exception('Failed to send email')
+    except Exception as e:
+        app.logger.error(f'Failed to send email: {str(e)}')
+        self.retry(exc=e)
+
+def send_email_with_message(user_email, redirect_url, email_subject, email_template, email_body):
+    service_name = os.getenv('SERVICE_NAME')
+    body = render_template(email_body, redirect_url=redirect_url, user_email=user_email, service_name=service_name)
+    html = render_template(email_template, redirect_url=redirect_url, user_email=user_email, service_name=service_name)
     
     # Generate the email, and send it.
     email = email_app.make_email(user_email, email_subject, body, html)
     send_async_email.delay(email)
-    app.logger.info("Email task queued for: " + user_email)
+    app.logger.info(f'Email task queued for: {user_email}')
 
 def send_email_verification_email(user_email):
-    send_email_with_message(user_email, 'email-verification-salt',
-                            'email_verification',
+    token = generate_token(user_email, salt='email-verification-salt', expiration=3600)
+    # redirect_url = get_external_url('email_verification', token=token)
+    redirect_url = f'{FRONTEND_URL}/email/verify/{token}'
+    send_email_with_message(user_email, redirect_url,
                             'Confirm Your Email',
                             'email_verification/verify-email.html', 'email_verification/verify-email.txt')
 
 def send_password_reset_email(user_email):
-    send_email_with_message(user_email, 'password-reset-salt',
-                            'set_new_password',
+    token = generate_token(user_email, salt='password-reset-salt', expiration=1800)
+    redirect_url = f'{FRONTEND_URL}/password/set-new/{token}'
+    send_email_with_message(user_email, redirect_url,
                             'Set New Password',
-                            'reset_password/email-reset.html', 'reset_password/email-reset.txt')
-
-def get_external_url(endpoint, **values):
-    return NGINX_URL + url_for(endpoint, **values)
-
-def maybe_load_user(user_email):
-    user_obj = None
-    if user_email == 'anonymous':
-        app.logger.info('Anonymous user detected')
-    elif user_email:
-        app.logger.info('User is authenticated, skipping CSRF header validation.')
-        user_obj = load_user(user_email)
-    return user_obj
+                            'reset_password/reset-password.html', 'reset_password/reset-password.txt')
 
 
 ####################
 ## LISTING ROUTES ##
 ####################
 
+def properties_response(properties_df, num_properties_per_page=1, page=1, user_obj=None):
+    saved_zpids = set(user_obj.saved) if user_obj else {}
+    response_data = properties_response_from_properties_df(properties_df, num_properties_per_page=num_properties_per_page, page=page, saved_zpids=saved_zpids)
+    # If the user is logged out, add a description to log in.
+    if user_obj:
+        response_data['descriptions']['Save'] = 'Save/unsave this property to go back to it later.'
+    else:
+        response_data['descriptions']['Save'] = 'To save a property you must first login.'
+    return response_data
+
+def search_properties(query_address):
+    # Filter the DataFrame for addresses that contain the query string, case-insensitive
+    return BACKEND_PROPERTIES_DF[BACKEND_PROPERTIES_DF['street_address'].str.contains(query_address, case=False, na=False)]
+
 @app.route('/api/test', methods=['GET'])
 def test_route():
-    return jsonify({
-        'msg': 'Hello from the backend!',
-        'status': 'success'
-    }), 200
+    return fancy_flash('Hello from the backend!', 'success', 'test', 'fadeIn'), 200
 
 @app.route('/api/explore', methods=['POST'])
 @jwt_required()
 def explore():
     # Retrieve the user identity if the session is authenticated
     user_email = get_jwt_identity()
-    app.logger.info(f"User email is: {user_email}")
     user_obj = maybe_load_user(user_email)
 
     request_data = request.get_json()
@@ -255,10 +258,9 @@ def search():
     page = int(request_data.get('current_page'))
     property_address = request_data.get('property_address', '')
     properties_df = search_properties(property_address)
-    
-    if properties_df.empty:
-        return jsonify({'msg': f"No search results include: '{property_address}'."}), 200
-    response_data = properties_response(properties_df, num_properties_per_page=min(len(properties_df), 10), page=page, user_obj=user_obj)
+    num_properties_per_page = max(min(len(properties_df), 10), 1)
+
+    response_data = properties_response(properties_df, num_properties_per_page=num_properties_per_page, page=page, user_obj=user_obj)
     response_json = json.dumps(response_data)
     return Response(response_json, mimetype='application/json')
 
@@ -271,10 +273,9 @@ def saved():
     request_data = request.get_json()
     page = int(request_data.get('current_page'))
     properties_df = BACKEND_PROPERTIES_DF.loc[list(user_obj.saved)]
-    
-    if properties_df.empty:
-        return jsonify({'msg': 'No saved properties.'}), 200
-    response_data = properties_response(properties_df, num_properties_per_page=min(len(properties_df), 10), page=page, user_obj=user_obj)
+    num_properties_per_page = max(min(len(properties_df), 10), 1)
+
+    response_data = properties_response(properties_df, num_properties_per_page=num_properties_per_page, page=page, user_obj=user_obj)
     response_json = json.dumps(response_data)
     return Response(response_json, mimetype='application/json')
 
@@ -363,7 +364,7 @@ def start_authenticated_session():
         - user_email: Email associated with an account.
         - user_password: Password associated with the given user_email.
     '''
-    app.logger.info("New request just came in")
+    app.logger.info('New request just came in.')
     data = request.get_json()
     user_email, user_password = data.get('user_email'), data.get('user_password')
 
@@ -388,7 +389,7 @@ def start_authenticated_session():
     response.set_cookie('csrf_access_token', csrf_access_token, httponly=True)
     set_refresh_cookies(response, refresh_token)
     response.set_cookie('csrf_refresh_token', csrf_refresh_token, httponly=True)
-    app.logger.info(f"Auth response is: {response}")
+    app.logger.info(f'Auth response is: {response}')
     return response, 200
 
 @app.route('/api/refresh-auth-session', methods=['POST'])
@@ -428,7 +429,7 @@ def profile():
     if not user:
         return jsonify({'msg': 'User not found'}), 404
 
-    app.logger.info(f"User info isss: {user}")
+    app.logger.info(f'User info isss: {user}.')
     user_info = {
         'email': user_email,
         'first_name': user['name'][0],
@@ -442,11 +443,11 @@ def register():
     data = request.get_json()
     user_email = data.get('userEmail')
 
-    # Check if email already exists
+    # Check if email already exists.
     if user_email in users:
         return jsonify(fancy_flash('Email already registered.', 'error', 'register', 'shake')), 200
 
-    # Add user to the 'database'
+    # Add user to the 'database'.
     users[user_email] = {
         'id': len(users) + 1,
         'name': (data.get('firstName'), data.get('lastName')),
@@ -460,55 +461,61 @@ def register():
     send_email_verification_email(user_email)
     return jsonify(fancy_flash('Please confirm your email address.', 'success', 'register', 'fadeIn')), 200
 
-
-@app.route('/api/email/verify/<token>')
+@app.route('/api/email/verify/<token>', methods=['POST'])
 def email_verification(token):
     user_email = verify_token(token, salt='email-verification-salt', expiration=3600)
     if user_email:
-        app.logger.info(f"We have decoded the email: {user_email}")
-        # Mark user's email as verified in your user model
+        app.logger.info(f'We have decoded the email: {user_email}, its type is: {type(token)}')
+        user_obj = maybe_load_user(user_email)
+        if user_obj:
+            users[user_email]['confirmed'] = True
+            return jsonify(fancy_flash('Email confirmed.', 'success', 'email-verify', 'fadeIn')), 200
+    return jsonify(fancy_flash('Unable to locate user associated with the given email address.', 'error', 'email-verify', 'shake')), 200
+
+@app.route('/api/password/request-new', methods=['POST'])
+def reset_password():
+    data = request.get_json()
+    user_email = data.get('userEmail')
+    user_obj = maybe_load_user(user_email)\
+
+    if user_obj:
+        app.logger.info(f'Reset password email is: {user_email}.')
+        send_password_reset_email(user_email)
+        return jsonify(fancy_flash('A password reset link has been sent to your email.', 'success', 'password-request-new', 'fadeIn')), 200
+    else:
+        app.logger.info(f'Users are: {users}')
+        return jsonify(fancy_flash('No account found with that email address.', 'error', 'password-request-new', 'shake')), 200
+
+@app.route('/api/password/set-new/<token>', methods=['POST'])
+def set_new_password(token):
+    try:
+        app.logger.info(f'Tokenizing: {token}, with type: {type(token)}')
+        user_email = verify_token(token, salt='password-reset-salt', expiration=1800)
+        app.logger.info(f'Password set new tokenized user: {user_email}')
+        if not user_email:
+            return jsonify(fancy_flash('No user email from the autodirected token.', 'error', 'password-set-new', 'shake')), 200
+
         user_obj = maybe_load_user(user_email)
         if not user_obj:
-            app.logger.info("No user found :<")
-            return redirect(f"{FRONTEND_URL}/email-verification?status=error&message=unknown_email")
-        users[user_email]['confirmed'] = True
-        return redirect(f"{FRONTEND_URL}/email-verification?status=info&message=email_confirmed")
-    return redirect(f"{FRONTEND_URL}/login")
+            app.logger.info(f'User not found, no users: {users}')
+            return jsonify(fancy_flash('No user associated with the given email.', 'error', 'password-set-new', 'shake')), 200
 
-# @app.route('/api/reset-password-request', methods=['POST'])
-# @jwt_required()
-# def reset_password():
-#     user_email = get_jwt_identity()
-#     user_obj = maybe_load_user(user_email)
+        new_password = request.get_json()['new_password']
+        app.logger.info(f'User is: {user_obj}, new_pass is: {new_password}')
+        users[user_email]['password'] = generate_password_hash(new_password)
+        return jsonify(fancy_flash('Your password has been updated.', 'success', 'password-set-new', 'fadeIn')), 200
+    except (SignatureExpired, BadSignature):
+        return jsonify(fancy_flash('The reset link is invalid or has expired.', 'error', 'password-set-new', 'shake')), 200
+    except KeyError:
+        return jsonify(fancy_flash('Invalid data received.', 'error', 'password-set-new', 'shake')), 200
 
-#     if user_obj:
-#         send_password_reset_email(user_email)
-#         return jsonify({'msg': 'A password reset link has been sent to your email', 'status': 'success'}), 200
-#     else:
-#         return jsonify({'msg': 'No account found with that email address', 'status': 'error'}), 409
+@app.route('/api/report/app-issue', methods=['POST'])
+def report():
+    request_data = request.get_json()
+    user_email = request_data.get('user_email', '')
+    issue_description = request_data.get('issue_description', '')
 
-# @app.route('api/set-new-password/<token>', methods=['POST'])
-# def set_new_password(token):
-#     try:
-#         user_email = verify_token(token, salt='password-reset-salt', expiration=1800)
-#         new_password = request.json['new_password']
-#         user = users.get(user_email)
-#         user['password'] = generate_password_hash(new_password)
-#         return flash_message('Your password has been updated.', category='success', area='set-new-password', animation='fadeIn'), 200
-#     except (SignatureExpired, BadSignature):
-#         return flash_message('The reset link is invalid or has expired.', category='error', area='set-new-password', animation='shake'), 400
-#     except KeyError:
-#         return flash_message('Invalid data received.', category='error', area='set-new-password', animation='shake'), 400
-
-# @app.route('/report', methods=['POST'])
-# def report():
-#     request_data = request.get_json()
-#     user_email = request_data.get('user_email', '')
-#     issue_description = request_data.get('issue_description', '')
-
-#     app.logger.info(f"{user_email} has filed an issue: {issue_description}.")
-    
-#     return jsonify({"success": "Issue reported successfully"}), 200
+    return jsonify(fancy_flash(f"You have reported the issue: '{issue_description[:10]}...'.", 'success', 'report', 'fadeIn')), 200
 
 
 
